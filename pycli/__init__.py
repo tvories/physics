6f676c129746df14fa52a8cfb9be1caa21f7ac26
Taylor Vories <taylor@tmtech.me>
2018-05-28 15:32:41 -0600

    fixed sensor names

--- pycli/__init__.py
+++ pycli/__init__.py
@@ -3,8 +3,8 @@ from w1thermsensor import W1ThermSensor
 sensors = []
 generate = False
 
-container_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"0000046654e4")
-ambient_temp = ambient_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"000005fafed1")
+ambient_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"0000046654e4")
+container_temp = ambient_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"000005fafed1")
 container_temp.name = "container_temp"
 ambient_temp.name = "ambient_temp"
 
