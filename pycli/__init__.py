c29234786f0298a44eea7d3ebe9ac901f79d59b3
Taylor Vories <taylor@tmtech.me>
2018-05-28 15:54:35 -0600

    fixed temp sensor

--- pycli/__init__.py
+++ pycli/__init__.py
@@ -4,7 +4,7 @@ sensors = []
 generate = False
 
 ambient_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"0000046654e4")
-container_temp = ambient_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"000005fafed1")
+container_temp = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20,"000005fafed1")
 container_temp.name = "container_temp"
 ambient_temp.name = "ambient_temp"
 
